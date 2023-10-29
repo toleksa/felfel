b03e0840666e5146fc4da780ee89312df58ca842
toleksa <toleksa@gmail.com>
2023-10-29 19:00:32 +0200

    prometheus patch

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt install -y docker.io
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
@@ -13,4 +13,5 @@ curl -fsSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install --create-namespace --namespace monitoring prometheus prometheus-community/kube-prometheus-stack
+kubectl patch prometheus prometheus-kube-prometheus-prometheus -n monitoring --type='json' -p='[{"op": "replace", "path": "/spec/serviceMonitorSelector", "value": {}}]'
 kubectl apply -f kube.yaml
